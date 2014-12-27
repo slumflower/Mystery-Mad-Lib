0a4544a6a9bf94dad67ae31d052243201ed82ca8
slumflower <fleurdetaudis@gmail.com>
2014-12-27 00:14:20 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+body {
+    background: url('http://www.greatvectors.com/preview/triangle-background.jpg') no-repeat center center fixed;
+    background-size: cover;
+  	padding-top: 80px;
+	padding-bottom: 80px;
+}
+
+.container {
+  padding: 20px 0px;
+  max-width: 500px;
+  background: white;
+}
+
+h1, h3 {
+    text-align: center;
+}
+
+
+form {
+   padding: 10px;
+}
+
+input {
+  background: #fff;
+  border: 1px solid #c6c7cc;
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, .1);
+  padding: 6px;
+  margin-top: 6px;
+  width: 100%;
+}
+
+#story {
+    padding: 30px 10px;
+}
+
+
+
